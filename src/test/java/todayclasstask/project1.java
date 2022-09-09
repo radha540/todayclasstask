454dcf65d327c0a3fea4a43d9137660ba25224dc
radha540 <rbalu2004@gmail.com>
2022-09-09 21:58:13 +0530

    radhacode

--- src/test/java/todayclasstask/project1.java
+++ src/test/java/todayclasstask/project1.java
@@ -2,6 +2,9 @@ package todayclasstask;
 
 public class project1 {
 public static void main(String[] args) {
+	System.out.println("test1");
+	System.out.println("test2");
+	System.out.println("test3");
 	
 }
 }
